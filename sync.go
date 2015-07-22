94c29a08242658c82a8394f7dfd15848d94d413e
Glyn Normington <gnormington@pivotal.io>
2015-07-22 13:55:32 +0100

    Fix up partially constructed submodules.
    Fix submodules on exit as well as entry. This will ensure that gosub
    sync no longer leave "semi-submodules" lying around.
    
    It is still helpful to fix submodules on entry too to ensure a good
    starting point for syncing. For example, if the user runs an old
    version of gosub, they may end up with "semi-submodules" which need
    fixing up.
    
    Note that the submodule's git repository is not deleted, although
    its path is temporarily removed from the git index.
    
    Signed-off-by: Glyn Normington <gnormington@pivotal.io>

--- sync.go
+++ sync.go
@@ -40,10 +40,17 @@ func sync(c *cli.Context) {
 		pkgRoots[root] = repo
 	}
 
-	existingSubmodules, err := detectExistingGoSubmodules(repo, gopath)
+	existingSubmodules, err := detectExistingGoSubmodules(repo, gopath, false)
 	if err != nil {
-		println("failed to detect existing submodules: " + err.Error())
-		os.Exit(1)
+		if fixErr := fixExistingSubmodules(repo); fixErr != nil {
+			println("failed to fix existing submodules: " + fixErr.Error())
+			os.Exit(1)
+		}
+		existingSubmodules, err = detectExistingGoSubmodules(repo, gopath, true)
+		if err != nil {
+			println("failed to detect existing submodules: " + err.Error())
+			os.Exit(1)
+		}
 	}
 
 	gitmodules := filepath.Join(repo, ".gitmodules")
@@ -167,18 +174,26 @@ func sync(c *cli.Context) {
 			os.Exit(1)
 		}
 	}
+
+	if err := fixExistingSubmodules(repo); err != nil {
+		println("failed to fix submodules: " + err.Error())
+		os.Exit(1)
+	}
 }
 
-func detectExistingGoSubmodules(repo string, gopath string) ([]string, error) {
+func detectExistingGoSubmodules(repo string, gopath string, printErrors bool) ([]string, error) {
 	srcPath := filepath.Join(gopath, "src")
 
 	submoduleStatus := exec.Command("git", "submodule", "status", srcPath)
 	submoduleStatus.Dir = repo
 
-	submoduleStatus.Stderr = os.Stderr
+	if printErrors {
+		submoduleStatus.Stderr = os.Stderr
+	}
 
 	statusOut, err := submoduleStatus.StdoutPipe()
 	if err != nil {
+		printErr(printErrors, "detectExistingGoSubmodules failed to get StdoutPipe: %s\n", err)
 		return nil, err
 	}
 
@@ -186,6 +201,7 @@ func detectExistingGoSubmodules(repo string, gopath string) ([]string, error) {
 
 	err = submoduleStatus.Start()
 	if err != nil {
+		printErr(printErrors, "detectExistingGoSubmodules failed to start git submodule status: %s\n", err)
 		return nil, err
 	}
 
@@ -202,6 +218,147 @@ func detectExistingGoSubmodules(repo string, gopath string) ([]string, error) {
 
 	err = submoduleStatus.Wait()
 	if err != nil {
+		printErr(printErrors, "detectExistingGoSubmodules failed to wait for git submodule status: %s\n", err)
+		return nil, err
+	}
+
+	return submodules, nil
+}
+
+func printErr(print bool, format string, err error) {
+	if print {
+		fmt.Printf(format, err)
+	}
+}
+
+// Convert any "semi-submodules" into first class submodules.
+// See http://stackoverflow.com/questions/4161022/git-how-to-track-untracked-content/4162672#4162672
+func fixExistingSubmodules(repo string) error {
+	submodules, err := getSubmodules(repo)
+	if err != nil {
+		return err
+	}
+
+	var lastErr error
+	for _, submodule := range submodules {
+		err = fixSubmodule(submodule)
+		if err != nil {
+			fmt.Printf("fixExistingSubmodules failed to fix submodule %s\n", submodule)
+			lastErr = err
+		}
+	}
+
+	return lastErr
+}
+
+func fixSubmodule(submodule string) error {
+	fmt.Printf("\x1b[31mFixing submodule %s .", submodule)
+	defer fmt.Println("\x1b[0m")
+	rm := exec.Command("git", "rm", "--cached", "-f", submodule)
+	rm.Stderr = os.Stderr
+	err := rm.Run()
+	if err != nil {
+		return fmt.Errorf("fixSubmodule failed to remove submodule path %s from the index: %s", submodule, err)
+	}
+	fmt.Printf(".")
+
+	url, err := submoduleUrl(submodule)
+	if err != nil {
+		return fmt.Errorf("fixSubmodule failed to determine URL of submodule %s: %s", submodule, err)
+	}
+
+	submoduleAdd := exec.Command("git", "submodule", "add", url, submodule)
+	submoduleAdd.Stderr = os.Stderr
+	err = submoduleAdd.Run()
+	if err != nil {
+		return fmt.Errorf("fixSubmodule failed to add submodule %s: %s", submodule, err)
+	}
+	fmt.Printf(".")
+
+	fmt.Printf(". done.")
+	return nil
+}
+
+func submoduleUrl(submodule string) (string, error) {
+	submoduleQuery := exec.Command("git", "remote", "show", "origin")
+	submoduleQuery.Dir = submodule
+	submoduleQuery.Stderr = os.Stderr
+
+	lsFileOut, err := submoduleQuery.StdoutPipe()
+	if err != nil {
+		fmt.Printf("submoduleUrl failed to get StdoutPipe: %s\n", err)
+		return "", err
+	}
+
+	lineScanner := bufio.NewScanner(lsFileOut)
+
+	err = submoduleQuery.Start()
+	if err != nil {
+		fmt.Printf("submoduleUrl failed to start git remote show origin: %s\n", err)
+		return "", err
+	}
+
+	var url string
+	for lineScanner.Scan() {
+		segments := strings.Fields(lineScanner.Text())
+
+		if len(segments) < 3 {
+			continue
+		}
+
+		if segments[0] == "Fetch" && segments[1] == "URL:" {
+			url = segments[2]
+		}
+	}
+
+	if url == "" {
+		return "", fmt.Errorf("submoduleUrl failed to find the URL of %s\n", submodule)
+	}
+
+	err = submoduleQuery.Wait()
+	if err != nil {
+		fmt.Printf("submoduleUrl failed to wait for git remote show origin: %s\n", err)
+		return "", err
+	}
+
+	return url, nil
+}
+
+func getSubmodules(repo string) ([]string, error) {
+	lsFiles := exec.Command("git", "ls-files", "--stage")
+	lsFiles.Dir = repo
+	lsFiles.Stderr = os.Stderr
+
+	lsFileOut, err := lsFiles.StdoutPipe()
+	if err != nil {
+		fmt.Printf("getSubmodules failed to get StdoutPipe: %s\n", err)
+		return nil, err
+	}
+
+	lineScanner := bufio.NewScanner(lsFileOut)
+
+	err = lsFiles.Start()
+	if err != nil {
+		fmt.Printf("getSubmodules failed to start git ls-files --stage: %s\n", err)
+		return nil, err
+	}
+
+	submodules := []string{}
+	for lineScanner.Scan() {
+		segments := strings.Fields(lineScanner.Text())
+
+		if len(segments) < 4 {
+			return nil, fmt.Errorf("invalid git ls-files output: %q", lineScanner.Text())
+		}
+
+		if segments[0] == "160000" {
+			submodules = append(submodules, segments[3])
+		}
+	}
+
+	err = lsFiles.Wait()
+	if err != nil {
+		fmt.Printf("getSubmodules failed to wait for git ls-files --stage: %s\n", err)
 		return nil, err
 	}
 
