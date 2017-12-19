7637833a6490f368d88ecddd027706fbbed9f878
Nima Kaviani <nkavian@us.ibm.com>
2017-12-19 10:29:29 -0800

    blow up if gosub fail
    otherwise it is really hard to tell if gosub has failed or not since it always
    exits with status code 0
    
    Signed-off-by: John Shahid <jvshahid@gmail.com>

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/codegangsta/cli"
@@ -72,5 +73,8 @@ func main() {
 		},
 	}
 
-	app.Run(os.Args)
+	if err := app.Run(os.Args); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
